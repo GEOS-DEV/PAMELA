f3c9a60dcd0baf51af6c5db64b2d16bd8275c7c9
Antoine Mazuyer <antoine.mazuyer@protonmail.com>
2018-07-30 15:11:52 -0700

    begim import vtk

--- PAMELA/source/MeshDataWriters/VTKWriter.hpp
+++ PAMELA/source/MeshDataWriters/VTKWriter.hpp
@@ -1,6 +1,8 @@
 #pragma once
 // Library includes
 #include "MeshDataWriters/MeshDataWriter.hpp"
+#include <vtkXMLPMultiBlockDataWriter.h>
+#include <vtkSmartPointer.h>
 
 #if defined( _WIN32)
 #include <direct.h>
@@ -11,12 +13,33 @@
 
 namespace PAMELA
 {
+    class VTKWriter : public MeshDataWriter{
+        public:
+            VTKWriter(Mesh * mesh, std::string name) : MeshDataWriter(mesh,name) {}
 
-	namespace VTK_
-	{
+            virtual void Init() final;
+            virtual void DumpVariables() final;
 
-		class VTKWriter;
+        private:
 
-	}
+            /*!
+             * @brief Make the parent file (.pvtu)
+             */
+            void MakeParentFile();
 
+            /*!
+             * @brief Make the children files (.vtu)
+             */
+            void MakeChildFiles();
+
+            /*!
+             * @brief Declare the variables which are on the Mesh on the
+             * VTK object
+             */
+            void DeclareAllVariables();
+
+        private:
+            /// VTU file
+            vtkSmartPointer<vtkXMLPMultiBlockDataWriter> vtm_ {vtkXMLPMultiBlockDataWriter::New()};
+    };
 }

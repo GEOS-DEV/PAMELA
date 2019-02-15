cfe5db0199e24cad3157702d43e1a1592e6384dd
Antoine Mazuyer <antoine.mazuyer@protonmail.com>
2019-02-15 15:48:02 -0800

    fix compilation problems

--- PAMELA/source/MeshDataWriters/Writer.hpp
+++ PAMELA/source/MeshDataWriters/Writer.hpp
@@ -6,8 +6,8 @@ namespace PAMELA
     class Writer : public MeshDataWriter{
         public:
             Writer(Mesh * mesh, std::string name) : MeshDataWriter(mesh,name) {}
-            virtual void Init() final {};
-            virtual void Dump() final {};
+            virtual void Init() final {}
+            virtual void Dump() final {}
 
             const PartMap<Polyhedron*> &  GetPolyhedronMap() const {
               return m_PolyhedronParts;
